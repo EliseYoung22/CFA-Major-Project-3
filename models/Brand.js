ff2aaaf3b395f8176f6f3722499cebd718100037
Elise Young <eliseyoung_4@hotmail.com>
2017-05-29 17:23:30 +1000

    inital commit with express and mongo

---
+++ models/Brand.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+//destructuring
+const { Schema } = mongoose;
+
+const brandSchema = new Schema ({
+  name: {
+    type: String,
+    trim: true
+  },
+  ethical: {
+    type: String,
+    trim: true
+  },
+  sustainable: {
+    type: String,
+    trim: true
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  certificates: {
+    type: String,
+    trim: true
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+
+const Brand = mongoose.model('Brand', brandSchema);
+
+module.exports = Brand;
---
+++ routes/index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+const Brand = require('../models/Brand')
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+router.get('/', function(req, res, next) {
+  Brand.find()
+  .then(brands => {
+    res.render('index', {
+      title: 'Brands',
+      brand: brand,
+    })
+  })
+});
+
+
+router.post('/', (req, res) => {
+  const name = req.body.brand_name;
+  const ethical = req.body.brand_ethical;
+  const sustainable = req.body.brand_sustainable;
+  const description = req.body.brand_description;
+  const certificates = req.body.brand_certificates;
+  let brand = new Brand();
+  brand.name = name;
+  brand.ethical = ethical;
+  brand.sustainable = sustainable;
+  brand.description = description;
+  brand.certificates = certificates;
+  brand.save()
+  .then(() => {
+    res.redirect('/')
+  })
+})
+
+module.exports = router;
